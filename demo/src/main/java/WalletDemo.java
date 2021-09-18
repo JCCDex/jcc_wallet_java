c370e899ef1f7829d62481c2e7ca27fa025eebab
xdjiang <jiangxudong1982@gmail.com>
2021-09-18 22:37:42 +0800

    更新demo

--- demo/src/main/java/WalletDemo.java
+++ demo/src/main/java/WalletDemo.java
@@ -1,16 +1,33 @@
 import com.jccdex.core.client.Wallet;
+import com.jccdex.core.client.WalletSM;
 
 public class WalletDemo {
     public static void main(String[] args) {
-        //创建新钱包
-        Wallet wallet = Wallet.generate();
-        System.out.println("创建新钱包:");
-        System.out.println(wallet.getSecret());
-        System.out.println(wallet.getAddress());
+        //------------------非国密版钱包----------------------
+        //创建公链新钱包
+        Wallet wallet1 = Wallet.generate();
+        System.out.println("创建公链钱包:");
+        System.out.println("钱包密钥:"+wallet1.getSecret());
+        System.out.println("钱包地址:"+wallet1.getAddress());
+        System.out.println("--------------------------------------------------------------");
         //通过密钥导出钱包
-        Wallet wallet1 = Wallet.fromSecret("shTesjei9WvYGXceiUgSFAixakVNo");
-        System.out.println("通过密钥导出钱包:");
-        System.out.println(wallet1.getSecret());
-        System.out.println(wallet1.getAddress());
+        Wallet wallet2 = Wallet.fromSecret("shTesjei9WvYGXceiUgSFAixakVNo");
+        System.out.println("通过密钥导出公链钱包:");
+        System.out.println("钱包密钥:"+wallet2.getSecret());
+        System.out.println("钱包地址:"+wallet2.getAddress());
+        System.out.println("--------------------------------------------------------------");
+        //创建联盟链钱包
+        Wallet wallet3= Wallet.generate("dpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcjeCg65rkm8oFqi1tuvAxyz");
+        System.out.println("创建联盟链(SEAA)钱包:");
+        System.out.println("钱包密钥:"+wallet3.getSecret());
+        System.out.println("钱包地址:"+wallet3.getAddress());
+        System.out.println("--------------------------------------------------------------");
+
+        //------------------非国密版钱包----------------------
+        //创建公链国密版新钱包
+        WalletSM wallet4 = WalletSM.generate();
+        System.out.println("创建公链国密版钱包:");
+        System.out.println("钱包密钥:"+wallet4.getSecret());
+        System.out.println("钱包地址:"+wallet4.getAddress());
     }
 }
